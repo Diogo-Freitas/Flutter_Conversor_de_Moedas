bb7656f7c4a80c45777934933de56ba184873225
Diogo-Freitas <diogofreitasdf@hotmail.com>
2024-12-05 08:12:32 -0300

    Conversor de Moedas

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:conversor_de_moedas/pages/my_home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Conversor de Moedas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: '\$ Conversor \$'),
+    );
+  }
+}
+
+
---
+++ lib/pages/my_home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+        title: Text(widget.title,
+            style: const TextStyle(fontWeight: FontWeight.w500)),
+      ),
+      body: const Center(),
+      backgroundColor: Colors.black,
+    );
+  }
+}
